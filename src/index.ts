8101896a5e94af9d512df8fd4ec85653eb3f2073
Yash Namdeo <yashnamdeo754@gmail.com>
2025-11-30 19:19:08 +0530

    refactor: restructure user management and message handling logic

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,40 @@ import { WebSocketServer } from "ws";
 
 const wss: WebSocketServer = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    roomId: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount++;
-    console.log(`New user connected. Total users: ${userCount}`);
+    console.log("New User Connected");
 
     socket.on("message", (message) => {
         console.log(`Received message: ${message.toString() }`);
-        allSockets.forEach( s => {
-            s.send(`Some User : ${message.toString() }`);
-        })
+        const msg = JSON.parse(message.toString());
+        if (msg.type === "join") {
+            const user: User = {
+                socket: socket,
+                roomId: msg.payload.roomId
+            };
+            allSockets.push(user);
+        }
+        if (msg.type === "chat"){
+            let currentUserRoomId = allSockets.find(user => user.socket === socket)?.roomId;
+            allSockets.forEach(user => {
+                if (user.roomId === currentUserRoomId){
+                    user.socket.send(JSON.stringify(msg.payload.message));
+                }
+            })
+        }
+        
+
     });
 
     socket.on("close", () =>{
-        userCount--;
-        allSockets = allSockets.filter( s => s !== socket);
-        console.log(`User disconnected. Total users: ${userCount}`);
+        console.log("User Disconnected");
+        
     })
 });
