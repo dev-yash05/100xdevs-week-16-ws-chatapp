276a22b606de25e36a65a1a8eecbf3e1e0d28faa
Yash Namdeo <yashnamdeo754@gmail.com>
2025-11-30 10:18:12 +0530

    simple starter code

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+
+wss.on("connection", (socket) => {
+    userCount++;
+    console.log(`New user connected. Total users: ${userCount}`);
+
+    socket.on("message", (message) => {
+        console.log(`Received message: ${message.toString() }`); 
+        setTimeout(()=>{
+            socket.send("message recieved: " + message.toString()); 
+        },1000);  
+    });
+
+    socket.on("close", () =>{
+        userCount--;
+        console.log(`User disconnected. Total users: ${userCount}`);
+    })
+});
