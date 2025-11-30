7fb5fdeafe30ba1baf9d8fdd6b7c4afc809f960f
Yash Namdeo <yashnamdeo754@gmail.com>
2025-11-30 11:18:50 +0530

    refactor: improve type annotations and enhance message broadcasting logic

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
+import type { WebSocket } from "ws";
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss: WebSocketServer = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount++;
     console.log(`New user connected. Total users: ${userCount}`);
 
     socket.on("message", (message) => {
         console.log(`Received message: ${message.toString() }`); 
         setTimeout(()=>{
-            socket.send("message recieved: " + message.toString()); 
+            socket.send("message recieved: " + message.toString());
+            for (let i = 0; i < allSockets.length; i++){
+                const s: any = allSockets[i];
+                s.send("broadcast: " + message.toString());
+            } 
         },1000);
     });
 
